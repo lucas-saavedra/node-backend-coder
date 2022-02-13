e99bb81a2e4aa16e6f3f002be9d8de5cc4b1a817
Lucas Saavedra <lucassaavedra50@gmail.com>
2022-02-13 20:38:24 -0300

    Entrega api REST

---
+++ helpers/ProductsHandler.js
@@ -0,0 +1,49 @@
+class ProductsHandler {
+    constructor(array) {
+        this.productList = array;
+    }
+    getAll() {
+        return this.productList;
+    }
+    getById(id) {
+        return this.productList.find(e => e.id === id);
+    }
+
+    addProduct(product) {
+        if (!this.productList.length) {
+            product.id = 1;
+        } else {
+            product.id = this.productList.at(-1).id + 1;
+        }
+        this.productList.push(product);
+        return product;
+    }
+    updateProduct(title, price, thumbnail, id) {
+        const index = this.productList.findIndex((product) => product.id === id);
+        if (index < 0) {
+            return { success: false, status: 404, error: `El producto con el id: ${id} no existe` };
+        } else {
+            const newProduct = {
+                ...this.productList[index],
+                title,
+                price,
+                thumbnail
+            }
+            this.productList[index] = newProduct;
+            return { success: true, result: newProduct };
+        }
+    }
+    deleteProdById(id) {
+        const index = this.productList.findIndex((product) => product.id === id);
+        if (index < 0) {
+            return { success: false, status: 404, error: `El producto con el id: ${id} no existe` };
+        } else {
+            this.productList = this.productList.filter((product) => product.id !== id);
+            return {
+                success: true, result: `El producto con el id: ${id} fue eliminado correctamente`
+            };
+        }
+    }
+};
+
+module.exports = ProductsHandler;
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const { Contenedor } = require('./helpers/Contenedor');
-const contenedor = new Contenedor('productos.txt');
+const apiRoutes = require('./routers/index')
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/productos', async (req, res) => {
-    res.json(await contenedor.getAll());
-});
 
-app.get('/productoRandom', async (req, res) => {
-    res.json(await contenedor.productoRandom());
-});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(PORT, () => {
+app.use(express.static(__dirname + '/public'))
+app.use('/api', apiRoutes)
+
+
+const connectedServer = app.listen(PORT, () => {
     console.log(`Listening at: ${PORT}`)
 });
+
+connectedServer.on('error', (error) => {
+    console.log('Error: ', error);
+})
 
